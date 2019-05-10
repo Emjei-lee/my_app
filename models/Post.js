4fb5fcd8c5e375bfd1b06f64dc3c8609f68adfc4
Emjei-lee <juniplayer@naver.com>
2019-05-10 11:59:16 +0900

    2019-05-10 : comment

--- models/Post.js
+++ models/Post.js
@@ -4,6 +4,13 @@ var postSchema = mongoose.Schema({
   title: {type:String, required:true},
   body: {type:String, required:true},
   author: {type:mongoose.Schema.Types.ObjectId, ref:'user', required:true},
+  views: {type:Number, default: 0},
+  numId: {type:Number, required:true},
+  comments:[{
+    body: {type:String, required:true},
+    author: {type:mongoose.Schema.Types.ObjectId, ref:'user', required:true},
+    createdAt: {type:Date, default:Date.now}
+  }],
   createdAt: {type:Date, default:Date.now},
   updatedAt: Date
 });
